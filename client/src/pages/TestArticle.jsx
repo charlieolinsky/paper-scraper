228d72bb80e62410990520a5d981d73756763b4b
Peter Navarrete <navarrep1@newpaltz.edu>
2024-06-13 14:30:41 -0400

    added react router for SPA rendering

--- client/src/pages/TestArticle.jsx
+++ client/src/pages/TestArticle.jsx
@@ -2,7 +2,6 @@ import { useState } from "react"
 import { getArticles, addArticle, updateArticle, deleteArticle } from "../api/article"
 
 export default function TestArticle() {
-
     const [articles, setArticles] = useState([])
 
     const onButtonClick = async () => {
@@ -15,16 +14,11 @@ export default function TestArticle() {
         console.log(error)
         error = await updateArticle("updated", "article is article", "size is size","a8f2b9b8-c73b-44a3-8332-ead4ecdbb836")
         console.log(error)
-
         response = await deleteArticle("4f8c2593-fadd-4780-9d45-57bd250c8da6")
         console.log(response)
-
     }
 
     return (
-
-
-
         <input type="button" onClick={onButtonClick} value={'Test Articles'} />
     )
 }
