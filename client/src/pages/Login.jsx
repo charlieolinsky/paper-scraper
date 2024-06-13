0dc8d22236e0fa314b601235590fe07555b39d3a
Peter Navarrete <navarrep1@newpaltz.edu>
2024-06-12 23:30:41 -0400

    init bare files, working login page.

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,46 @@
+import { SupabaseClient } from "@supabase/supabase-js"
+import { useState } from "react"
+import supabase from "../utils/supabase"
+
+export default function Login() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const onButtonClick = async () => {
+        console.log(email)
+        console.log(password)
+        
+        const {data, err} = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        })
+
+        if (err != null) {
+            console.log(err)
+        }
+
+        console.log("user signed in")
+        console.log(data)
+    }
+
+    return (
+        <form id="loginForm">
+            <label>
+                <input 
+                    value={email}
+                    placeholder="Enter email"
+                    onChange={(ev) => setEmail(ev.target.value)}
+                />
+            </label>
+            <label>
+                <input
+                    value={password}
+                    placeholder="Enter your password here"
+                    onChange={(ev) => setPassword(ev.target.value)}
+                />
+            </label>
+
+            <input type="button" onClick={onButtonClick} value={'Log in'} />
+        </form>
+    )
+}
