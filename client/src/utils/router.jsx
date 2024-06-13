e3557eb19e5c84de7ebf7efc9dc7e7859b57cb7e
Peter Navarrete <navarrep1@newpaltz.edu>
2024-06-13 14:54:41 -0400

    router

--- client/src/utils/router.jsx
+++ client/src/utils/router.jsx
@@ -1,10 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
-    }
+    },
+    {
+        path: "/login",
+        element: <Login/>,
+    },
+    {
+        path: "/signup",
+        element: <Signup/>,
+    },
+    {
+        path: "/home",
+        element: <App />,
+    },
+
 
 ])
